f7254c29807253eec311606023dea395bd58700f
Agung Indrawan <1ndrww09@gmail.com>
2024-04-06 10:59:22 +0800

    update build

--- lib/data.ts
+++ lib/data.ts
@@ -1,14 +1,3 @@
-import { prisma } from "@/lib/prisma";
-
-export const getContacts = async () => {
-    try {
-        const contacts = await prisma.contact.findMany();
-        return contacts
-    } catch (error) {
-        throw new Error("Failed to fetch contact data")
-    }
-}
-
 export const getProfile = () => {
    const user  = {
     id : "11",
